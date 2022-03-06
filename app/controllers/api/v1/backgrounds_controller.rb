67df1460bb57a22a93074e8e83403cd942a1ee2b
John Napier <jcee.napier@gmail.com>
2022-03-05 23:12:50 -0700

    Complete 1b background photo api consumption

---
+++ app/controllers/api/v1/backgrounds_controller.rb
@@ -0,0 +1,7 @@
+class Api::V1::BackgroundsController < ApplicationController 
+  def index 
+    photo = UnsplashFacade.location_photo(params["location"])
+
+    render(json: PhotoSerializer.new(photo))
+  end
+end
--- app/facades/unsplash_facade.rb
+++ app/facades/unsplash_facade.rb
@@ -2,6 +2,6 @@ class UnsplashFacade
   def self.location_photo(city)
     data = UnsplashService.location_photo(city)
 
-    Photo.new(data)
+    Photo.new(data[:results][0])
   end
 end
