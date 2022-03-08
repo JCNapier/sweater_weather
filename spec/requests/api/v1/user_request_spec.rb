2c626da5bf047fe3d1377b324c26e6e4689877f7
John Napier <jcee.napier@gmail.com>
2022-03-08 12:41:10 -0700

    Add sad path testing

--- spec/requests/api/v1/user_request_spec.rb
+++ spec/requests/api/v1/user_request_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'Creating a User' do
-  it 'successfully creates a user', :vcr do 
+  it 'happy path successfully creates a user', :vcr do 
     user_params = { 
       "email": "johnthefisherman@gmail.com",
       "password": "ToTheMoon",
@@ -21,5 +21,47 @@ RSpec.describe 'Creating a User' do
     expect(user_json[:data][:attributes]).to be_a(Hash)
     expect(user_json[:data][:attributes][:email]).to be_a(String)
     expect(user_json[:data][:attributes][:api_key]).to be_a(String)
+  end
+
+  it 'sad path unsuccessfully creates a user unmatching passwords', :vcr do 
+    user_params = { 
+      "email": "johnthefisherman@gmail.com",
+      "password": "ToTheLake",
+      "password_confirmation": "ToTheMoon"
+    }
+   
+    post '/api/v1/users',  params: user_params
+    
+    user_json = JSON.parse(response.body, symbolize_names: true)
+    
+    expect(response).to be_successful
+    expect(response.status).to eq(200)
+
+    expect(user_json).to be_a(Hash)
+    expect(user_json[:message]).to be_a(String)
+    expect(user_json[:message]).to eq("Passwords Do Not Match")
+  end
+
+  it 'sad path unsuccessfully creates a user with existing email', :vcr do 
+    user = (build :user)
+    
+    user_params = { 
+      "email": "#{user.email}",
+      "password": "ToTheMoon",
+      "password_confirmation": "ToTheMoon"
+    }
+   
+    post '/api/v1/users',  params: user_params
+
+    post '/api/v1/users',  params: user_params
+    
+    user_json = JSON.parse(response.body, symbolize_names: true)
+    
+    expect(response).to be_successful
+    expect(response.status).to eq(200)
+
+    expect(user_json).to be_a(Hash)
+    expect(user_json[:message]).to be_a(String)
+    expect(user_json[:message]).to eq("Email Already Exists")
   end
 end
