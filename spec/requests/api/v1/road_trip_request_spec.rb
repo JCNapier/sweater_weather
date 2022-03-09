87867759bf6effefeaa19fdda61f475ab9c8c4ed
John Napier <jcee.napier@gmail.com>
2022-03-09 15:51:45 -0700

    Add API key validation for road trip request spec

--- spec/requests/api/v1/road_trip_request_spec.rb
+++ spec/requests/api/v1/road_trip_request_spec.rb
@@ -2,11 +2,12 @@ require 'rails_helper'
 
 RSpec.describe 'road trip request spec' do
   it 'happy path returns a road trip with weather conditions', :vcr do 
-
+    user = User.create!(email: "superman@dailyplanet.com", password_digest: "Clark Kent")
+    
     road_trip_params = {
       "origin": "Denver,CO",
       "destination": "Pueblo,CO",
-      "api_key": "jgn983hy48thw9begh98h4539h4"
+      "api_key": "#{user.api_key}"
     }
     
     post '/api/v1/road_trip', params: road_trip_params
@@ -27,11 +28,12 @@ RSpec.describe 'road trip request spec' do
   end
 
   it 'sad path for locaitons spanning unpassable routes', :vcr do 
+    user = User.create!(email: "superman@dailyplanet.com", password_digest: "Clark Kent")
     
     road_trip_params = {
       "origin": "New York, NY",
       "destination": "London, UK",
-      "api_key": "jgn983hy48thw9begh98h4539h4"
+      "api_key": "#{user.api_key}"
     }
 
     post '/api/v1/road_trip', params: road_trip_params
@@ -45,11 +47,12 @@ RSpec.describe 'road trip request spec' do
   end
 
   it 'sad path for missing params', :vcr do 
+    user = User.create!(email: "superman@dailyplanet.com", password_digest: "Clark Kent")
     
     road_trip_params = {
       "origin": "",
       "destination": "London, UK",
-      "api_key": "jgn983hy48thw9begh98h4539h4"
+      "api_key": "#{user.api_key}"
     }
 
     post '/api/v1/road_trip', params: road_trip_params
@@ -61,4 +64,24 @@ RSpec.describe 'road trip request spec' do
     expect(trip_json[:error]).to be_a(String)
     expect(trip_json[:error]).to eq("At least two locations must be provided.")
   end
+
+  it 'sad path for incorrect api_key', :vcr do 
+    user = User.create!(email: "superman@dailyplanet.com", password_digest: "Clark Kent")
+
+    road_trip_params = {
+      "origin": "Denver,CO",
+      "destination": "Pueblo,CO",
+      "api_key": "jgn983hy48thw9begh98h4539h4"
+    }
+
+    post '/api/v1/road_trip', params: road_trip_params
+
+    trip_json = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response.status).to eq(400)
+    expect(trip_json).to be_a(Hash)
+    expect(trip_json[:error]).to be_a(String)
+    expect(trip_json[:error]).to eq("A valid API key must be provided.")
+  end
+
 end
